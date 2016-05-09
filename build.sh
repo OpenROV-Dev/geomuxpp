1c2be5ee1928d0193ac8f872dd6ff6c86df02ffc
Charles Cross <charles@openrov.com>
2016-05-08 20:12:22 -0700

    Added JSON library to dependencies

--- build.sh
+++ build.sh
@@ -7,10 +7,12 @@ pushd deb
 wget http://openrov-software-nightlies.s3-us-west-2.amazonaws.com/jessie/zmq/openrov-zmq_1.0.0-1~1_armhf.deb
 wget http://openrov-software-nightlies.s3-us-west-2.amazonaws.com/jessie/ffmpeg/openrov-ffmpeg-lib_3.0.2~14_armhf.deb
 wget http://openrov-software-nightlies.s3-us-west-2.amazonaws.com/jessie/geocamera-libs/openrov-geocamera-libs_1.0.0-1~35.16a26aa_armhf.deb
+wget http://openrov-software-nightlies.s3-us-west-2.amazonaws.com/jessie/json/openrov-nlohmann-json_1.0.0-1~1_armhf.deb
 
 dpkg -i openrov-zmq_1.0.0-1~1_armhf.deb
 dpkg -i openrov-ffmpeg-lib_3.0.2~14_armhf.deb
 dpkg -i openrov-geocamera-libs_1.0.0-1~35.16a26aa_armhf.deb
+dpkg -i openrov-nlohmann-json_1.0.0-1~1_armhf.deb
 
 popd
 rm -rf deb
